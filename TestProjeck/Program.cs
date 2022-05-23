3b1035c40beab567bd0a6935874427008fbf4137
aeresilf <egurnovaelena56@gmail.com>
2022-05-23 09:37:22 +0500

    new1

--- TestProjeck/Program.cs
+++ TestProjeck/Program.cs
@@ -10,8 +10,11 @@ namespace TestProjeck
             int x = 0;
             int y = 0;
             double z = 0;
+            double n = 0;
             z = x/y;
             Console.WriteLine(z);
+            n = x+y;
+            Console.WriteLine(n);
         }
     }
 }

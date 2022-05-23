86b143f396f2f1be18b840879230167955d2630e
aeresilf <egurnovaelena56@gmail.com>
2022-05-23 09:27:33 +0500

    Добавьте файлы проекта.

---
+++ TestProjeck/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestProjeck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 0;
+            int y = 0;
+            double z = 0;
+            z = x/y;
+            Console.WriteLine(z);
+        }
+    }
+}
